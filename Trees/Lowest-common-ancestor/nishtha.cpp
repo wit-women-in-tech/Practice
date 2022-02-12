c910d9bc26bffdbe98f0d481585d9c0d542ac6c3
Nishtha-Goyal <nishtha2242@gmail.com>
2022-02-12 20:03:09 +0530

    nishtha

---
+++ Trees/Lowest-common-ancestor/nishtha.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)return NULL;
+        if(root==p || root==q) return root;
+        TreeNode*l=lowestCommonAncestor(root->left,p,q);
+            TreeNode*r=lowestCommonAncestor(root->right,p,q);
+        if(l && r)return root;
+        if(!r)return l;
+        if( !l) return r;
+        return NULL;
+    }
+};
