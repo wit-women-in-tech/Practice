c0efb4c1811ef2e15b64b04c412a848d170c5973
Nishtha-Goyal <nishtha2242@gmail.com>
2022-02-12 20:32:04 +0530

    nishtha-goyal

--- Trees/Construct-BST-from-preorder/nishtha.cpp
+++ Trees/Construct-BST-from-preorder/nishtha.cpp
@@ -1,42 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<double> averageOfLevels(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        
-        q.push(NULL);
-        vector<double> ans;
-        if(!root)return ans;
-        while(!q.empty() && q.front()!=NULL)
+    TreeNode*helper(vector<int>& pre,int si,int ei)
+    {
+        if(ei<si)return NULL;
+        TreeNode* root=new TreeNode(pre[si]);
+        int i=si;
+        for( i=si;i<=ei;i++)
         {
-            double c=0;
-            long long int av=0;
-            while(q.front())
-            {
-                
-                av+=q.front()->val;
-                if(q.front()->left)q.push(q.front()->left);
-                if(q.front()->right)q.push(q.front()->right);
-                q.pop();
-                c++;
-            }
-            q.pop();
-            q.push(NULL);
-            double t=av/c;
-           
-            ans.push_back(t);
+            if(pre[i]>pre[si])break;
         }
-        return ans;
+        root->left=helper(pre,si+1,i-1);
+        root->right=helper(pre,i,ei);
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& pre) {
+      return helper(pre,0,pre.size()-1);
     }
 };
